73bfca747db03233ba795d8deddcc1f0f008578c
Ryan <ryan.matthews@colorado.edu>
2016-04-07 18:29:47 -0600

    Commented python script

--- data-retrieval/beerScript.py
+++ data-retrieval/beerScript.py
@@ -4,29 +4,39 @@ import urllib2
 import re
 import sys
 
+
+
+#Extracts the name of the beer from an appropriate beersmith recipe url.
 def url_to_beer_name(url):
 	parsed_url= url.split('/')
 	return parsed_url[-1].replace('-', ' ').title()
 
+#Finds the search term specified after the executable and stores it in search.
 parser = argparse.ArgumentParser(description = "Process Search Term")
 parser.add_argument('searchTerm')
 args = parser.parse_args()
 search = args.searchTerm
+
+#Converts the search term into the appropriate beersmithrecipes.com url.
 search = search.replace(' ', '+')
 search = "http://beersmithrecipes.com/searchrecipe?uid=&term=" + search
+
+#Extracts the html from the url stored in search and stores it in html.
 html = urllib2.urlopen(search)
+
+#uses beautifulsoup to extract all the links from the html and stores them in links if they are a recipe url.
 soup = BeautifulSoup(html.read())
-
-
 links = []
 for link in soup.find_all('a'):
 	if "viewrecipe" in link.get('href'):
 		links.append(link.get('href'))
 
+#Ends program when no recipe urls are found.
 if len(links)==0:
 	print "No results,try again."
 	sys.exit()
 
+#Prompts the user to pick one of the beers that appeared from the search
 print 'Which beer recipe do you want to choose? Type in the appropriate number, from 1 to ' + str(len(links))
 iterator=1
 parser = []
@@ -42,9 +52,12 @@ while(True):
 	else:
 		print "Invalid response, try again."
 
+#Extracts the html of the chosen beer url are stores it into html.
 html = urllib2.urlopen(search)
 soup = BeautifulSoup(html.read())
+print '\n' + url_to_beer_name(search) + '\n'
 
+#Finds the Bitterness and ABV values for the appropriate beer and prints it out
 trs = soup.find_all('tr')
 BittInd = 0;
 ABVInd = 0;
